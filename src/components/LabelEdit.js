20cad08a61386f4e6998a6282227cb0e5ffd6f37
Thomas Castle <thomascastle.dvlp@gmail.com>
2021-12-18 19:35:40 +0630

    Label name cannot be blank

--- src/components/LabelEdit.js
+++ src/components/LabelEdit.js
@@ -7,6 +7,7 @@ import {
   ButtonPrimary,
   FormGroup,
   IssueLabelToken,
+  PointerBox,
   StyledOcticon,
   TextInput,
 } from "@primer/react";
@@ -19,6 +20,7 @@ export function LabelEdit({ label, on, onCancel: cancel }) {
   const [color, setColor] = React.useState(`#${label.color}` ?? "#ededed");
   const [shouldDisable, setShouldDisable] = React.useState(false);
   const inputColor = React.useRef(null);
+  const [placeholder, setPlaceholder] = React.useState(label.name);
 
   const getProvidedOrDefaultColor = (value) => {
     return isValidColorHex(value) ? value : "#ededed";
@@ -32,7 +34,7 @@ export function LabelEdit({ label, on, onCancel: cancel }) {
     setColor(generateRandomColor());
   };
 
-  const [updateLabel, { loading }] = useMutation(MUTATION_UPDATE_LABEL, {
+  const [updateLabel, { error, loading }] = useMutation(MUTATION_UPDATE_LABEL, {
     optimisticResponse: {
       updateLabel: {
         label: {
@@ -54,8 +56,15 @@ export function LabelEdit({ label, on, onCancel: cancel }) {
     },
   });
 
-  const saveChanges = async () => {
-    await updateLabel();
+  const handleFormSubmitted = async (e) => {
+    e.preventDefault();
+
+    try {
+      await updateLabel();
+    } catch (e) {
+      return null;
+    }
+
     cancel();
   };
 
@@ -82,7 +91,7 @@ export function LabelEdit({ label, on, onCancel: cancel }) {
       <Box sx={{ width: ["75%", "75%", "25%"] }}>
         <IssueLabelToken
           fillColor={getProvidedOrDefaultColor(color)}
-          text={name}
+          text={placeholder}
           size="large"
         />
       </Box>
@@ -119,104 +128,136 @@ export function LabelEdit({ label, on, onCancel: cancel }) {
           width: "100%",
         }}
       >
-        <Box
-          sx={{
-            alignItems: ["flex-start", null, "flex-end"],
-            display: "flex",
-            flexDirection: ["column", null, "row"],
-            mb: -2,
-          }}
-        >
-          <FormGroup
-            sx={{
-              my: [2, 2, 3],
-              pr: [0, 1, 3],
-              width: ["100%", "100%", "25%"],
-            }}
-          >
-            <FormGroup.Label htmlFor="label-name">Label name</FormGroup.Label>
-            <TextInput
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-              placeholder="Label name"
-              sx={{ width: "100%" }}
-              value={name}
-            />
-          </FormGroup>
-          <FormGroup
-            sx={{
-              flex: "auto",
-              my: [2, 2, 3],
-              pr: [0, 1, 3],
-              width: ["100%", "100%", "25%", "33.33333%"],
-            }}
-          >
-            <FormGroup.Label htmlFor="label-description">
-              Description
-            </FormGroup.Label>
-            <TextInput
-              onChange={(e) => {
-                setDescription(e.target.value);
-              }}
-              placeholder="Description (optional)"
-              sx={{ width: "100%" }}
-              value={description}
-            />
-          </FormGroup>
-          <FormGroup
-            sx={{ my: [2, 2, 3], width: ["100%", "100%", "16.66667%"] }}
-          >
-            <FormGroup.Label htmlFor="label-color">Color</FormGroup.Label>
-            <Box sx={{ display: "flex" }}>
-              <Button
-                onClick={getNewColor}
-                sx={{ flexShrink: 0, mr: 2, px: "7px" }}
-              >
-                <StyledOcticon icon={SyncIcon} />
-              </Button>
-              <TextInput
-                autocomplete="off"
-                maxLength="7"
-                onChange={handleColorChanged}
-                pattern="#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})"
-                ref={inputColor}
-                required
-                title="Hex colors should only contain numbers and letters from a-f"
-                value={color}
-              />
-            </Box>
-          </FormGroup>
+        <form method="post" onSubmit={handleFormSubmitted}>
           <Box
             sx={{
+              alignItems: ["flex-start", null, "flex-end"],
               display: "flex",
-              justifyContent: ["flex-start", "flex-start", "flex-end"],
-              ml: [0, 0, 5],
-              my: [2, 2, 3],
-              width: ["100%", "100%", "33.33333%", "25%"],
+              flexDirection: ["column", null, "row"],
+              mb: -2,
             }}
           >
-            <Button
-              onClick={() => {
-                cancel();
-              }}
+            <FormGroup
               sx={{
-                color: "btn.text",
-                mr: [0, 0, 2],
-                order: [2, 2, 1],
+                my: [2, 2, 3],
+                pr: [0, 1, 3],
+                width: ["100%", "100%", "25%"],
               }}
             >
-              Cancel
-            </Button>
-            <ButtonPrimary
-              disabled={shouldDisable}
-              onClick={saveChanges}
-              sx={{ mr: [2, null, 0], order: [1, null, 2] }}
+              <FormGroup.Label
+                htmlFor="label-name"
+                sx={{
+                  color: error ? "danger.fg" : "fg.default",
+                }}
+              >
+                Label name
+              </FormGroup.Label>
+              <TextInput
+                onChange={(e) => {
+                  setName(e.target.value);
+                  setPlaceholder(
+                    e.target.value.length > 0 ? e.target.value : "Label preview"
+                  );
+                }}
+                placeholder="Label name"
+                required
+                sx={{ width: "100%" }}
+                validationStatus={error ? "error" : "none"}
+                value={name}
+              />
+              {error ? (
+                <PointerBox
+                  bg="danger.subtle"
+                  borderColor="danger.muted"
+                  caret="top-left"
+                  sx={{
+                    mt: 2,
+                    mx: 0,
+                    position: "absolute",
+                    px: 2,
+                    py: 1,
+                  }}
+                >
+                  {error.message}
+                </PointerBox>
+              ) : null}
+            </FormGroup>
+            <FormGroup
+              sx={{
+                flex: "auto",
+                my: [2, 2, 3],
+                pr: [0, 1, 3],
+                width: ["100%", "100%", "25%", "33.33333%"],
+              }}
             >
-              {!loading ? "Save changes" : "Saving..."}
-            </ButtonPrimary>
+              <FormGroup.Label htmlFor="label-description">
+                Description
+              </FormGroup.Label>
+              <TextInput
+                onChange={(e) => {
+                  setDescription(e.target.value);
+                }}
+                placeholder="Description (optional)"
+                sx={{ width: "100%" }}
+                value={description}
+              />
+            </FormGroup>
+            <FormGroup
+              sx={{ my: [2, 2, 3], width: ["100%", "100%", "16.66667%"] }}
+            >
+              <FormGroup.Label htmlFor="label-color">Color</FormGroup.Label>
+              <Box sx={{ display: "flex" }}>
+                <Button
+                  onClick={getNewColor}
+                  sx={{ flexShrink: 0, mr: 2, px: "7px" }}
+                  type="button"
+                >
+                  <StyledOcticon icon={SyncIcon} />
+                </Button>
+                <TextInput
+                  autocomplete="off"
+                  maxLength="7"
+                  onChange={handleColorChanged}
+                  pattern="#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})"
+                  ref={inputColor}
+                  required
+                  title="Hex colors should only contain numbers and letters from a-f"
+                  value={color}
+                />
+              </Box>
+            </FormGroup>
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: ["flex-start", "flex-start", "flex-end"],
+                ml: [0, 0, 5],
+                my: [2, 2, 3],
+                width: ["100%", "100%", "33.33333%", "25%"],
+              }}
+            >
+              <Button
+                onClick={() => {
+                  cancel();
+                }}
+                sx={{
+                  color: "btn.text",
+                  mr: [0, 0, 2],
+                  order: [2, 2, 1],
+                }}
+                type="button"
+              >
+                Cancel
+              </Button>
+              <ButtonPrimary
+                disabled={shouldDisable}
+                sx={{ mr: [2, null, 0], order: [1, null, 2] }}
+                type="submit"
+              >
+                {!loading ? "Save changes" : "Saving..."}
+              </ButtonPrimary>
+            </Box>
           </Box>
-        </Box>
+        </form>
       </Box>
     </>
   );
